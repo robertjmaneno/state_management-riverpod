4ba9c551498fcc085028e7a7d0053dcbcb4faa50
Robert Maneno <robertjmaneno@gmail.com>
2025-01-17 11:02:49 +0200

    added watch provider

---
+++ lib/providers/product_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$productsHash() => r'48c888b003496256deeeaee20811324bf5f8ab85';
+
+/// See also [products].
+@ProviderFor(products)
+final productsProvider = AutoDisposeProvider<List<Product>>.internal(
+  products,
+  name: r'productsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$productsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ProductsRef = AutoDisposeProviderRef<List<Product>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
